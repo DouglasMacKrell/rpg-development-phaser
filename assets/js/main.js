98b681261e940b6ca3c8035b33f76fa2c5a1d4af
DouglasMacKrell <douglasmackrell@pursuit.org>
2020-08-16 22:41:00 -0400

    added first sprite and image to canvas

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,13 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-function preload() {}
+function preload() {
+  this.load.image("button1", "assets/images/ui/blue_button01.png");
+}
 
-function create() {}
+function create() {
+  var button = this.add.image(100, 100, "button1");
+  button.setOrigin(0.5, 0.5);
+
+  this.add.sprite(300, 100, "button1")
+}
